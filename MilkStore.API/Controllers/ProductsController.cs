dc7802239769143fa48c534f0e1a0be2f1074946
Chí Đạt <153970055+Dat213DZ@users.noreply.github.com>
2024-09-19 22:14:14 +0700

    Khong su dung var, them getproduct name

--- MilkStore.API/Controllers/ProductsController.cs
+++ MilkStore.API/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using MilkStore.Contract.Repositories.Entity;
 using MilkStore.Contract.Services.Interface;
+using MilkStore.Core;
 using MilkStore.Core.Base;
 using MilkStore.ModelViews.ProductsModelViews;
 using MilkStore.ModelViews.UserModelViews;
@@ -20,6 +21,7 @@ namespace MilkStore.API.Controllers
         {
             _ProductsService = ProductsService;
         }
+
         [HttpGet]
         //[Authorize(Roles = "Admin,Member")]
         public async Task<IActionResult> GetProducts(string? id)
@@ -27,7 +29,7 @@ namespace MilkStore.API.Controllers
             try
             {
                 // Lấy tất cả sản phẩm
-                var products = await _ProductsService.GetProducts(id);
+                IEnumerable<ProductsModel> products = await _ProductsService.GetProducts(id);
 
                 // Kiểm tra xem có sản phẩm nào không
                 if (products == null || !products.Any())
@@ -42,13 +44,37 @@ namespace MilkStore.API.Controllers
                 return StatusCode(500); // Trả về mã lỗi 500
             }
         }
+
+        [HttpGet("GetByName")]
+        public async Task<IActionResult> GetByName(string? name)
+        {
+            if (name == null)
+            {
+                return BadRequest("Tìm kiếm rỗng!!!");
+            }
+            else
+            {
+                IEnumerable<ProductsModel> product = await _ProductsService.GetProductsName(name);
+
+                if (product == null || !product.Any())
+                {
+                    return BadRequest("Không tìm thấy sản phẩm!!!");
+                }
+                else
+                {
+                    return Ok(product);
+                }
+            }
+        }
+
         [HttpGet("GetPagging")]
         //[Authorize(Roles = "Admin,Member")]
         public async Task<IActionResult> Paging(int index, int size)
         {
-            var paging = await _ProductsService.PagingProducts(index, size);
+            BasePaginatedList<Products> paging = await _ProductsService.PagingProducts(index, size);
             return Ok(paging);
         }
+
         [HttpPost()]
         //[Authorize(Roles = "Admin")]
         public async Task<IActionResult> CreateProducts(ProductsModel ProductsModel)
@@ -57,9 +83,11 @@ namespace MilkStore.API.Controllers
             {
                 return BadRequest(new BaseException.BadRequestException("BadRequest", ModelState.ToString()));
             }
+
             Products Products = await _ProductsService.CreateProducts(ProductsModel);
             return Ok(BaseResponse<Products>.OkResponse(Products));
         }
+
         [HttpPut("{id}")]
         //[Authorize(Roles = "Admin")]
         public async Task<IActionResult> UpdateProduct(string id, [FromBody] ProductsModel productsModel)
@@ -71,7 +99,7 @@ namespace MilkStore.API.Controllers
 
             try
             {
-                var updatedProduct = await _ProductsService.UpdateProducts(id, productsModel);
+                Products updatedProduct = await _ProductsService.UpdateProducts(id, productsModel);
                 return Ok(updatedProduct);
             }
             catch (Exception ex)
@@ -79,13 +107,14 @@ namespace MilkStore.API.Controllers
                 return NotFound(new { message = ex.Message });
             }
         }
+
         [HttpDelete("{id}")]
         //[Authorize(Roles = "Admin")]
         public async Task<IActionResult> DeleteProduct(string id)
         {
             try
             {
-                var deletedProduct = await _ProductsService.DeleteProducts(id);
+                Products deletedProduct = await _ProductsService.DeleteProducts(id);
                 return Ok(deletedProduct); // Trả về sản phẩm đã bị xóa
             }
             catch (Exception ex)
