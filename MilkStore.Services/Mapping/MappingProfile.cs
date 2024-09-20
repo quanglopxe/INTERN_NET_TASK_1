9e47526bb0e598b2feb87fcf59d0df14b9cbb4d8
davidnguyennq <nguyennhatquan.dev@gmail.com>
2024-09-20 21:07:11 +0700

    Update Voucher (Using Mapper)

---
+++ MilkStore.Services/Mapping/MappingProfile.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using MilkStore.Contract.Repositories.Entity;
+using MilkStore.ModelViews.ResponseDTO;
+using MilkStore.ModelViews.OrderModelViews;
+using MilkStore.ModelViews.ProductsModelViews;
+using MilkStore.ModelViews.UserModelViews;
+using MilkStore.Repositories.Entity;
+using MilkStore.ModelViews.CategoryModelViews;
+using MilkStore.ModelViews.ReviewsModelView;
+using MilkStore.ModelViews.PreOrdersModelView;
+using MilkStore.ModelViews.PostModelViews;
+using MilkStore.ModelViews.VoucherModelViews;
+
+
+namespace MilkStore.Services.Mapping
+{
+
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Category, CategoryModel>();
+            CreateMap<CategoryModel,Category>();            
+
+            #region Post
+            CreateMap<PostModelView, Post>();
+            CreateMap<Post, PostResponseDTO>()
+                .ForMember(dest => dest.Products, opt => opt.MapFrom(src => src.PostProducts.Select(pp => new ProductResponseDTO
+                {
+                    ProductID = pp.Product.Id,
+                    ProductName = pp.Product.ProductName,
+                    Description = pp.Product.Description,
+                    Price = pp.Product.Price,
+                    QuantityInStock = pp.Product.QuantityInStock,
+                    ImageUrl = pp.Product.ImageUrl
+                }).ToList()));
+            #endregion
+
+            CreateMap<Products, ProductsModel>();
+            CreateMap<ProductsModel, Products>();
+
+            CreateMap<UserModelView, ApplicationUser>()
+            .ForMember(dest => dest.Points, opt => opt.MapFrom(src => 0)) // Set Points to 0
+            .ForMember(dest => dest.CreatedTime, opt => opt.MapFrom(src => DateTimeOffset.UtcNow));
+
+            CreateMap<UserModelView, ApplicationUser>()
+            .ForMember(dest => dest.LastUpdatedBy, opt => opt.Ignore())
+            .ForMember(dest => dest.LastUpdatedTime, opt => opt.Ignore())
+            .ForMember(dest => dest.Points, opt => opt.Ignore()); // Nếu không muốn ánh xạ Points
+
+            CreateMap<OrderModelView, Order>();
+            CreateMap<Order, OrderResponseDTO>()
+                .ForMember(dest => dest.OrderDetailss, opt => opt.MapFrom(src => src.OrderDetailss));
+            CreateMap<OrderDetails, OrderDetailResponseDTO>();
+
+            // Ánh xạ từ OrderModelView sang Order, nhưng chỉ cập nhật các thuộc tính thay đổi
+            CreateMap<OrderModelView, Order>()
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null)); // Chỉ ánh xạ khi giá trị mới khác null
+
+            CreateMap<Review, ReviewsModel>().ReverseMap();
+            CreateMap<PreOrders, PreOrdersModelView>().ReverseMap();
+
+            CreateMap<Voucher, VoucherModelView>().ReverseMap();
+        }
+    }
+}
