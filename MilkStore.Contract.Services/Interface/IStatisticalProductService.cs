e54879bf1d963cc4d5bb2bc8202f187ef752a22d
NhatQuin <153624811+QuinQueeee@users.noreply.github.com>
2024-10-03 19:05:24 +0700

    add service for statistical product

---
+++ MilkStore.Contract.Services/Interface/IStatisticalProductService.cs
@@ -0,0 +1,16 @@
+﻿using MilkStore.ModelViews.ResponseDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilkStore.Contract.Services.Interface
+{
+    public interface IStatisticalProductService
+    {
+        Task<List<ProductRevenueDTO>> GetRevenueByProduct(DateTime? startDate, DateTime? endDate);
+        Task<List<ProductSalesDTO>> GetBestWorstSellingProducts(DateTime? startDate, DateTime? endDate);
+        Task<List<ProductStockDTO>> GetLowStockProducts(int threshold);
+    }
+}
---
+++ MilkStore.ModelViews/ResponseDTO/ProductStockDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilkStore.ModelViews.ResponseDTO
+{
+    public class ProductStockDTO
+    {
+        public string Id { get; set; } 
+        public string ProductName { get; set; } 
+        public int QuantityInStock { get; set; } 
+        public string CategoryId { get; set; } 
+    }
+}
